3d54c54f8c9928bfb232089ef3fcd1879a95dc3b
Mike Monteith <mike@mikemonteith.com>
2024-04-29 11:05:23 +0100

    Use eleventy collections for sidebar nav
    Instead of manually creating a list of pages for the sidebar navigation,
    we can use elevent'y collection feature.
    Any page with the "component" tag will show up in the component sidebar nav
    
    Also some refactoring to simplify the "active" logic.

--- .eleventy.js
+++ .eleventy.js
@@ -39,32 +39,6 @@ export default function (eleventyConfig) {
     }
   })
 
-  eleventyConfig.addFilter(
-    'addActiveAttribute',
-    function (config, filePathStem) {
-      if (config.items) {
-        return {
-          ...config,
-          items: config.items.map((item) => ({
-            ...item,
-            active: filePathStem.indexOf(item.href) > -1
-          }))
-        }
-      } else if (config.sections) {
-        return {
-          ...config,
-          sections: config.sections.map((section) => ({
-            ...section,
-            items: section.items.map((item) => ({
-              ...item,
-              active: filePathStem.indexOf(item.href) > -1
-            }))
-          }))
-        }
-      }
-    }
-  )
-
   // We need this HtmlBase plugin for serving our docs on github pages at a subdirectory
   // https://www.11ty.dev/docs/plugins/html-base/
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
--- docs/components/card-links.md
+++ docs/components/card-links.md
@@ -1,6 +1,8 @@
 ---
 layout: layouts/component.njk
 title: Card links
+tags:
+  - component
 ---
 
 <p class="nhsuk-body-l">Use card links to help users reach the next stage of their NHS App journey.</p>
