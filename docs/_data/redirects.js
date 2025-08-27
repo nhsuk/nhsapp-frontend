b4a4fa64d8f0826b9276b55711af3ec22b4258d1
David Hunter <thebeakking@gmail.com>
2025-08-27 16:56:35 +0100

    lint

--- docs/_data/redirects.js
+++ docs/_data/redirects.js
@@ -4,8 +4,17 @@
  * permalink produces /path/index.html
  */
 export default [
-  { from: '/components/panel/', to: 'https://service-manual.nhs.uk/design-system/components/panel' },
-  { from: '/patterns/confirmation-page/', to: 'https://service-manual.nhs.uk/design-system/patterns/confirmation-page' },
-  { from: '/get-started/github-and-heroku/', to: '/get-started/nhsapp-prototype/' }
+  {
+    from: '/components/panel/',
+    to: 'https://service-manual.nhs.uk/design-system/components/panel'
+  },
+  {
+    from: '/patterns/confirmation-page/',
+    to: 'https://service-manual.nhs.uk/design-system/patterns/confirmation-page'
+  },
+  {
+    from: '/get-started/github-and-heroku/',
+    to: '/get-started/nhsapp-prototype/'
+  }
   // Add more redirects here as needed
 ]
